41c8e9c5356b964dc432c94a16cbb0758d230d31
Jeremy Otterstad <jotterstad@trinityinsight.com>
2017-04-20 15:38:11 -0500

    port change, provisioner

--- provision.sh
+++ provision.sh
@@ -19,9 +19,18 @@ cd /home/ubuntu && express --view=pug web01 && cd web01 && npm install
 cd /home/ubuntu && express --view=pug web02 && cd web02 && npm install   
 
 ## Node - Port Changes
+cp /home/vagrant/data/web01/www /home/ubuntu/web01/bin/
+cp /home/vagrant/data/web02/www /home/ubuntu/web02/bin/
+
+## Node - PM2
+sudo pm2 startup
+pm2 start /home/ubuntu/web00/bin/www
+pm2 start /home/ubuntu/web01/bin/www
+pm2 start /home/ubuntu/web02/bin/www
 
 ## HaProxy
 sudo add-apt-repository -y ppa:vbernat/haproxy-1.7
 sudo apt-get update
 sudo apt-get install -y haproxy
+sudo cp /home/vagrant/provision/haproxy.cfg /etc/haproxy/
 sudo service haproxy restart
